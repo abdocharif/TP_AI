1a852301d2d0182ae89e2115dbfcf22c453ee925
abdocharif <163608465+abdocharif@users.noreply.github.com>
2024-06-03 23:40:08 +0100

    ex9.py

--- ex9.py
+++ ex9.py
@@ -1,1 +1,9 @@
-nbr = int
+n = int(input("saisir un nombre  "))
+s=0
+for i in range(1,n):
+    if(n%i==0):
+        s +=i
+if s== n:
+    print ("le nombre est parfait")
+else:
+    print ("le nombre est pas parfait ")
